f66a3316dddb58b7f0ac553bfb14cd93a0e8d4fb
Kal Mondal <who.ru2.shout@gmail.com>
2021-10-11 13:45:43 +0530

    NavBar Added

---
+++ dist/scripts/navbar.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+const menuButton = header.querySelector(".menu");
+const navList = header.querySelector("ul");
+const navItems = Array.from(navList.children);
+
+const toggleNavList = () => {
+	navList.classList.toggle("active");
+	header.classList.toggle("fixed");
+};
+console.log(header);
+
+menuButton.addEventListener("click", toggleNavList);
+
+navItems.forEach((n) => {
+	n.addEventListener("click", toggleNavList);
+});
